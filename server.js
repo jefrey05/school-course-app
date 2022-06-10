5a49bd25df96e941cf799a5b7c80d0444168bde4
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-06-10 19:21:55 -0400

    beginning front end work on app

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const PORT = 2121;
 require('dotenv').config();
+const cors = require('cors');
+app.use(cors())
 
 let db,
 dbConnectionString = process.env.DB_STRING,
@@ -10,8 +12,19 @@ dbName = 'courses-db';
 
 MongoClient.connect(dbConnectionString,{useUnifiedTopology: true})
 .then(client=>{
-    console.log(`connected to ${dbName} Database`)
+    console.log(`connected to ${dbName} Database`);
+    db = client.db(dbName);
 })
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
+
 
 app.listen(process.env.PORT || PORT,()=>{
 console.log(`Server is running on port ${PORT}`)
