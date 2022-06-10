3e43c729c644282aa882699dcbfc908159d690f2
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-06-10 18:53:05 -0400

    establish connection between server and  mongo database

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ require('dotenv').config();
 let db,
 dbConnectionString = process.env.DB_STRING,
 dbName = 'courses-db';
+
+MongoClient.connect(dbConnectionString,{useUnifiedTopology: true})
+.then(client=>{
+    console.log(`connected to ${dbName} Database`)
+})
+
+app.listen(process.env.PORT || PORT,()=>{
+console.log(`Server is running on port ${PORT}`)
+})
